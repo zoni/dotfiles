9a939237484c543ca40852b8d0ede64bb5aa18b4
Nick Groenen <nick@groenen.me>
2016-11-05 18:53:11 +0100

    git: Set desktop-specific settings via postinstall

---
+++ postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+git config --global diff.tool meld
+git config --global merge.tool meld
+git config --global merge.conflictstyle diff3
+git config --global commit.gpgsign true
+git config --global commit.gpgsign true
+git config --global user.signingkey 4F0AD019928AE098
