1eac0a0c5a20c9fb68bc9f4d7b6018a4ef258cfc
Nick Groenen <nick@byte.nl>
2016-11-08 14:18:42 +0100

    vim: install puppet syntax if possible

--- postinstall.sh
+++ postinstall.sh
@@ -7,5 +7,7 @@ git config --global diff.tool meld
 git config --global merge.tool meld
 git config --global merge.conflictstyle diff3
 git config --global commit.gpgsign true
-git config --global commit.gpgsign true
 git config --global user.signingkey 4F0AD019928AE098
+if [ $(type -P vim-addon-manager) ]; then
+	vim-addon-manager install puppet
+fi
