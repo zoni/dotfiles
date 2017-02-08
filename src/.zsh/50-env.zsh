d562571256b82ea282688d5680d8616afcc617e3
Nick Groenen <nick@groenen.me>
2017-02-08 09:54:39 +0100

    zsh: setup password store env vars

--- src/.zsh/50-env.zsh
+++ src/.zsh/50-env.zsh
@@ -1,6 +1,9 @@
 export EDITOR="vim"
 export PAGER="less"
 
+export PASSWORD_STORE_DIR="$HOME/Documents/.password-store"
+export PASSWORD_STORE_GENERATED_LENGTH=32
+
 if [ -e "/usr/bin/lesspipe" ]; then
 	export LESSOPEN='| /usr/bin/lesspipe %s'
 	export LESSCLOSE='/usr/bin/lesspipe %s %s'
