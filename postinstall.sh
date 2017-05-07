27b43e16cb88613bbbb0f3e5210cd1a9070399e2
Nick Groenen <nick@groenen.me>
2017-05-07 16:02:09 +0200

    FreeBSD compatibility

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 set -x
 
 vim +PlugInstall +qall
-ln --symbolic --no-dereference --force $HOME/.vim/ $HOME/.config/nvim
-ln --symbolic --no-dereference --force $HOME/.vimrc $HOME/.config/nvim/init.vim
+ln -snf $HOME/.vim/ $HOME/.config/nvim
+ln -snf $HOME/.vimrc $HOME/.config/nvim/init.vim
