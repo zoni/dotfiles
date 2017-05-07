46349334043c645896e6638f41de6cfbb4ca939d
Nick Groenen <nick@groenen.me>
2017-05-07 16:03:10 +0200

    Don't install vim puppet syntax with vim-addon-manager

--- postinstall.sh
+++ postinstall.sh
@@ -8,6 +8,3 @@ git config --global merge.tool meld
 git config --global merge.conflictstyle diff3
 git config --global commit.gpgsign true
 git config --global user.signingkey 4F0AD019928AE098
-if [ $(type -P vim-addon-manager) ]; then
-	vim-addon-manager install puppet
-fi
