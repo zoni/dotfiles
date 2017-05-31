2c9d6e36d24e79c40736cf7be2c87abf6cbd03ed
Nick Groenen <nick@groenen.me>
2017-05-31 10:59:02 +0200

    Add mutt configuration

--- postinstall.sh
+++ postinstall.sh
@@ -8,3 +8,8 @@ git config --global merge.tool meld
 git config --global merge.conflictstyle diff3
 git config --global commit.gpgsign true
 git config --global user.signingkey 4F0AD019928AE098
+
+if [[ $USER == "zoni" ]]; then
+	EMAIL_PASSWORD="$(pass show Email/nick@groenen.me | head -n 1)"
+	sed -i -e "s/__PASSWORD__/${EMAIL_PASSWORD}/g" $HOME/.mutt/account
+fi
