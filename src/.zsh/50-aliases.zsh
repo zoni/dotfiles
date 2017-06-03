04d474cdb98808d4324ddf821b70be61c67c7b2a
Nick Groenen <nick@groenen.me>
2017-06-04 01:25:05 +0200

    zsh: add print-color-table alias

--- src/.zsh/50-aliases.zsh
+++ src/.zsh/50-aliases.zsh
@@ -23,14 +23,15 @@ alias p="pushd"
 alias P="popd"
 alias d='dirs -v | head -10'
 
+alias dotfiles.sh="dotfiles.sh --dotfiles-dir ~/.dotfiles"
 alias gpg="gpg2 --keyid-format long"
-alias xclip="xclip -selection clipboard"
+alias histgrep="history 0 | grep"
+alias notify-on-complete='notify-send --urgency=normal --icon ${${?/0/terminal}//<->*/error} "Command finished${${?/0/}//<->*/ with error}" ${history[$HISTCMD]%[;&|]*}'
+alias print-color-table='for i in {0..255} ; do printf "\x1b[38;5;${i}mcolor${i}\n" ; done | column'
+alias psgrep="ps aux | grep"
+alias ssh-nomaster="ssh -S none"
+alias stream2chromecast="stream2chromecast -port 32872"
 alias tmux="tmux -2"
 alias ts="tmuxsession"
-alias histgrep="history 0 | grep"
-alias psgrep="ps aux | grep"
 alias unison="unison -ui text"
-alias notify-on-complete='notify-send --urgency=normal --icon ${${?/0/terminal}//<->*/error} "Command finished${${?/0/}//<->*/ with error}" ${history[$HISTCMD]%[;&|]*}'
-alias ssh-nomaster="ssh -S none"
-alias dotfiles.sh="dotfiles.sh --dotfiles-dir ~/.dotfiles"
-alias stream2chromecast="stream2chromecast -port 32872"
+alias xclip="xclip -selection clipboard"
