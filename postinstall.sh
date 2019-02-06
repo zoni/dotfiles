8d096f78d264473712d2c6048f57dfce2e910464
Nick Groenen <nick.groenen@castoredc.com>
2019-02-06 14:41:33 +0100

    git: update signing key for work account

--- postinstall.sh
+++ postinstall.sh
@@ -7,7 +7,11 @@ git config --global diff.tool meld
 git config --global merge.tool meld
 git config --global merge.conflictstyle diff3
 git config --global commit.gpgsign true
-git config --global user.signingkey 4F0AD019928AE098
+if [[ $USER == "work" ]]; then
+	git config --global user.signingkey E1B292AF00FB1524D5B7292E93EC881C83165FCE
+else
+	git config --global user.signingkey 4F0AD019928AE098
+fi
 
 if [[ $USER == "zoni" ]]; then
 	EMAIL_PASSWORD="$(pass show Email/nick@groenen.me | head -n 1)"
