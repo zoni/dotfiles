4844c6ef76ab036a0138e6cd0a43fd878dad963e
Nick Groenen <nick.groenen@castoredc.com>
2019-05-10 15:35:42 +0200

    Append ruby gem path to $PATH

--- postinstall.sh
+++ postinstall.sh
@@ -6,3 +6,7 @@ set -x
 vim +PlugInstall +qall
 ln -snf $HOME/.vim/ $HOME/.config/nvim
 ln -snf $HOME/.vimrc $HOME/.config/nvim/init.vim
+
+# Avoid shelling out to ruby on each shell startup by substituting the ruby
+# call result at the time of dotfiles install.
+echo 'export PATH="$PATH:'"$(ruby -e 'puts Gem.user_dir')"'/bin"' >> $HOME/.zsh/05-path.zsh
