542ca6a4bea762333e96379981745440657316a8
Nick Groenen <nick@groenen.me>
2020-03-07 21:46:24 +0100

    zsh: simplify $PATH setup

--- src/.zsh/05-path.zsh
+++ src/.zsh/05-path.zsh
@@ -2,13 +2,7 @@
 # Do this before oh-my-zsh starts so that plugins all find the
 # correct executables.
 
-# This one may not always exist, so check to make sure (even though
-# non-existant directories in $PATH shouldn't matter).
-if [ -d "$HOME/.local/bin" ]; then
-	export PATH="$PATH:$HOME/.local/bin"
-fi
-
-export PATH="/usr/local/go/bin/:$HOME/Bin:$PATH:/usr/share/virtualenvwrapper:$HOME/.cargo/bin:$HOME/Workspace/go/bin"
+export PATH="$HOME/Bin:$PATH:/usr/local/go/bin/:/usr/share/virtualenvwrapper:$HOME/.cargo/bin:$HOME/Workspace/go/bin"
 export GOPATH="$HOME/Workspace/go"
 
 # Extra completions
