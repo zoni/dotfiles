a61e4921ad0734456f298736bf54468cf5d52236
Nick Groenen <nick@groenen.me>
2020-03-08 13:14:06 +0100

    zsh: remove broken .zsh/ symlinks

--- postinstall.sh
+++ postinstall.sh
@@ -7,6 +7,11 @@ vim +PlugInstall +qall
 ln -snf $HOME/.vim/ $HOME/.config/nvim
 ln -snf $HOME/.vimrc $HOME/.config/nvim/init.vim
 
+for file in "$HOME"/.zsh/*; do
+	# Remove broken symlinks
+	test -e "$file" || rm -f "$file"
+done
+
 # Avoid shelling out to ruby on each shell startup by substituting the ruby
 # call result at the time of dotfiles install.
 echo 'export PATH="$PATH:'"$(ruby -e 'puts Gem.user_dir')"'/bin"' >> $HOME/.zsh/05-path.zsh
