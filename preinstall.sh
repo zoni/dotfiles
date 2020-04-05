315163af496d91602738f766afc2a4cc2302bac7
Nick Groenen <nick.groenen@castoredc.com>
2020-04-05 19:55:15 +0200

    Replace xwindows programs with wayland utilities

--- preinstall.sh
+++ preinstall.sh
@@ -7,5 +7,7 @@ mkdir -p \
 	$HOME/.i3 \
 	$HOME/.vimperator \
 	$HOME/.local/share/applications \
+	$HOME/.config/autostart \
+	$HOME/.config/environment.d \
 	$HOME/.config/systemd/user \
 	$HOME/.vscode
