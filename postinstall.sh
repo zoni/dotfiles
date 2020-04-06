d3ee9044ae2ce95c53865234decde157f0ff5b23
Nick Groenen <nick.groenen@castoredc.com>
2020-04-06 16:40:52 +0200

    Run +PlugInstall in nvim when available

--- postinstall.sh
+++ postinstall.sh
@@ -3,9 +3,14 @@
 set -e
 set -x
 
-vim +PlugInstall +qall
 ln -snf $HOME/.vim/ $HOME/.config/nvim
 ln -snf $HOME/.vimrc $HOME/.config/nvim/init.vim
+
+if type nvim > /dev/null; then
+	nvim +PlugInstall +qall
+else
+	vim +PlugInstall +qall
+fi
 
 for file in "$HOME"/.zsh/*; do
 	# Remove broken symlinks
