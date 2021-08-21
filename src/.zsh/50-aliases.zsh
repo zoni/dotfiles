48a2cfe0f2924835e4e63f4422dfdfd4ef1140e2
Nick Groenen <nick@groenen.me>
2021-08-21 15:05:51 +0200

    Knowledgebase sitemap-select

--- src/.zsh/50-aliases.zsh
+++ src/.zsh/50-aliases.zsh
@@ -23,18 +23,35 @@ if type nvim > /dev/null; then
 fi
 
 if type knowledgebase > /dev/null; then
+	function knowledgebase_binary() {
+		# Helper function to determines the real knowledgebase binary path,
+		# used to avoid endless self-recursion because we're redefining
+		# "knowledgebase" as a function (and aliasing kbX to it).
+		#
+		# Calling /bin/which avoids shell built-ins otherwise we'd still get
+		# the function alias here.
+		/bin/which knowledgebase
+	}
+	function knowledgebase_sitemap_select() {
+		URL="$($(knowledgebase_binary) sitemap "$@" | rofi -dmenu -no-custom -i)"
+		if [[ $? -ne 0 ]]; then; return; fi
+
+		if [[ -v WAYLAND_DISPLAY ]]; then
+			printf "$URL" | wl-copy
+		else
+			printf "$URL" | xclip -selection clipboard
+		fi
+		notify-send "URL copied to clipboard" "$URL"
+	}
 	function knowledgebase() {
 		if [ "$#" -lt 1 ]; then
 			cd "$HOME/Knowledgebase"
 			git status --short
+		elif [[ "$1" == "sitemap-select" ]]; then
+			shift
+			knowledgebase_sitemap_select "$@"
 		else
-			# Determine the real knowledgebase binary path to avoid endless
-			# self-recursion.  Calling /bin/which avoids shell built-ins
-			# otherwise we'd still get the function alias.
-			#
-			# This trickery is needed because aliases are expanded when a
-			# function definition is read, not when the function is executed.
-			$(/bin/which knowledgebase) "$@"
+			$(knowledgebase_binary) "$@"
 		fi
 	}
 	alias kb=knowledgebase
