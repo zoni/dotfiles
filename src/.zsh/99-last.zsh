54f315e95e8fb7522ee75415a4879c22af91c4bb
Nick Groenen <nick@groenen.me>
2021-09-04 13:18:44 +0200

    zsh: asdf setup for pacman

---
+++ src/.zsh/99-last.zsh
@@ -0,0 +1,5 @@
+# https://asdf-vm.com/guide/getting-started.html#_3-install-asdf states:
+#
+#   > asdf scripts need to be sourced after you have set your $PATH and after you
+#   > have sourced your framework (oh-my-zsh etc).
+[[ -e /opt/asdf-vm/asdf.sh ]] && source /opt/asdf-vm/asdf.sh
