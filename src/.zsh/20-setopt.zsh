4fcbc436f9fc7aa771b5fe5daf0c029dd214644c
Nick Groenen <nick@groenen.me>
2022-06-19 12:30:11 +0200

    zsh: split out completions

--- src/.zsh/20-setopt.zsh
+++ src/.zsh/20-setopt.zsh
@@ -135,25 +135,3 @@ setopt bsd_echo
 unsetopt beep
 
 # }}}
-
-# {{{ Completions
-#
-# See also https://thevaluable.dev/zsh-completion-guide-examples/
-
-# Make completion matches case-insensitive
-#zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-
-# Make completion matches case-insensitive and match on partial words
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-# Enable a menu for completions
-zstyle ':completion:*' menu yes select
-
-# Make menu completion of files use the same colors as ls (set via $LS_COLORS)
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-
-# Group completions by source
-zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %d --%f'
-zstyle ':completion:*' group-name ''
-
-# }}}
