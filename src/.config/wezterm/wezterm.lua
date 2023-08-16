7f25ebfb0bd074511335d13e70253c89882aa535
Nick Groenen <nick.groenen@castoredc.com>
2023-08-16 09:44:02 +0200

    wezterm: use config_builder

--- src/.config/wezterm/wezterm.lua
+++ src/.config/wezterm/wezterm.lua
@@ -1,19 +1,30 @@
 local wezterm = require 'wezterm'
+local config = {}
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
-return {
-  check_for_updates = false,
-  color_scheme = "OneDark",
-  font = wezterm.font_with_fallback({
-    "Ubuntu Mono derivative Powerline",
-    "Noto Color Emoji",
-    "Symbols Nerd Font Mono",
-    "Symbola",
-    "Last Resort High-Efficiency",
-  }),
-  font_size = 12.0,
-  hide_tab_bar_if_only_one_tab = true,
-  scrollback_lines = 20000,
-  term = "wezterm",
-  window_background_opacity = 0.8,
-  window_close_confirmation = 'NeverPrompt',
-}
+config.check_for_updates = false
+config.term = "wezterm"
+
+config.window_close_confirmation = 'NeverPrompt'
+config.warn_about_missing_glyphs = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font_size = 12.0
+config.font = wezterm.font_with_fallback({
+  "Ubuntu Mono derivative Powerline",
+  "Noto Color Emoji",
+  "Symbols Nerd Font Mono",
+  "Symbola",
+  "Last Resort High-Efficiency",
+})
+
+config.window_background_opacity = 0.8
+config.color_scheme = "OneDark"
+
+config.scrollback_lines = 20000
+
+return config
